a012bcaa25cd2938fd5fabb820c515d95c0dd3cd
Harman Singh <singhharmank25@gmail.com>
2025-04-08 00:23:31 -0400

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8081;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(__dirname + '/public'));
 // Set EJS view engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
