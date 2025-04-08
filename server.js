a52ccb2b0cc0f89146be38facb6a91eaa02c221e
Harman Singh <singhharmank25@gmail.com>
2025-04-08 00:21:32 -0400

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 // Middleware
 app.use(express.json());
