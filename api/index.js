22de197d5d8422a2e87fdcf876afefc650c5c4d2
Harman Singh <singhharmank25@gmail.com>
2025-04-10 04:24:04 -0400

    update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-const serverless = require("serverless-http");
-const app = require("../server");
+const serverless = require('serverless-http');
+const app = require('../server'); // assuming your existing file is named server.js
 module.exports = serverless(app);
