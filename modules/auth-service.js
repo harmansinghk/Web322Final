e0962b43e4f9e7cde0a1cc11e1583946d51990f3
Harman Singh <singhharmank25@gmail.com>
2025-04-10 05:02:51 -0400

    .

--- modules/auth-service.js
+++ modules/auth-service.js
@@ -3,120 +3,85 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 let User;
+let dbConnection;
 
-// Schema
 const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        unique: true
-    },
+    userName: { type: String, unique: true },
     password: String,
     email: String,
-    loginHistory: [{
-        dateTime: Date,
-        userAgent: String
-    }]
+    loginHistory: [{ dateTime: Date, userAgent: String }]
 });
 
-// Initialize DB
-function initialize() {
-    return new Promise((resolve, reject) => {
-        const db = mongoose.createConnection(process.env.MONGODB);
+// Lazy initialization 
+async function ensureInitialized() {
+    if (User) return;
 
-        db.on("error", (err) => reject(err));
-
-        db.once("open", () => {
-            User = db.model("users", userSchema);
-            resolve();
+    if (!dbConnection) {
+        dbConnection = await mongoose.createConnection(process.env.MONGODB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
-    });
+    }
+
+    User = dbConnection.model("users", userSchema);
 }
 
-module.exports.initialize = initialize;
-
-// Auto-initialize for serverless (e.g., Vercel)
-initialize().catch((err) => {
-    console.error("MongoDB initialization failed:", err);
-});
-
-module.exports.registerUser = function (userData) {
-    return new Promise((resolve, reject) => {
-        if (!User) {
-            reject("User model not initialized.");
-            return;
-        }
-
-        if (userData.password !== userData.password2) {
-            reject("Passwords do not match");
-            return;
-        }
-
-        bcrypt
-            .hash(userData.password, 10)
-            .then((hash) => {
-                userData.password = hash;
-
-                let newUser = new User(userData);
-                newUser.save()
-                    .then(() => resolve())
-                    .catch((err) => {
-                        if (err.code === 11000) {
-                            reject("User Name already taken");
-                        } else {
-                            reject("There was an error creating the user: " + err);
-                        }
-                    });
-            })
-            .catch((err) => {
-                console.error("bcrypt error:", err);
-                reject("There was an error encrypting the password");
-            });
-    });
+// Init for local development
+module.exports.initialize = () => {
+    return ensureInitialized();
 };
 
-module.exports.checkUser = function (userData) {
-    return new Promise((resolve, reject) => {
-        if (!User) {
-            reject("User model not initialized.");
-            return;
+module.exports.registerUser = async function (userData) {
+    try {
+        await ensureInitialized();
+
+        if (userData.password !== userData.password2) {
+            throw "Passwords do not match";
         }
 
-        User.find({ userName: userData.userName })
-            .then((users) => {
-                if (users.length === 0) {
-                    reject(`Unable to find user: ${userData.userName}`);
-                    return;
-                }
+        const hash = await bcrypt.hash(userData.password, 10);
+        userData.password = hash;
 
-                bcrypt.compare(userData.password, users[0].password)
-                    .then((result) => {
-                        if (!result) {
-                            reject(`Incorrect Password for user: ${userData.userName}`);
-                            return;
-                        }
+        const newUser = new User(userData);
+        await newUser.save();
+    } catch (err) {
+        if (err.code === 11000) {
+            throw "User Name already taken";
+        } else if (typeof err === "string") {
+            throw err;
+        } else {
+            console.error("registerUser error:", err);
+            throw "There was an error creating the user.";
+        }
+    }
+};
 
-                        // Maintain login history
-                        if (users[0].loginHistory.length === 8) {
-                            users[0].loginHistory.pop();
-                        }
+module.exports.checkUser = async function (userData) {
+    try {
+        await ensureInitialized();
 
-                        users[0].loginHistory.unshift({
-                            dateTime: new Date().toString(),
-                            userAgent: userData.userAgent
-                        });
+        const users = await User.find({ userName: userData.userName });
+        if (users.length === 0) throw `Unable to find user: ${userData.userName}`;
 
-                        User.updateOne(
-                            { userName: users[0].userName },
-                            { $set: { loginHistory: users[0].loginHistory } }
-                        )
-                            .then(() => resolve(users[0]))
-                            .catch((err) => {
-                                reject("There was an error verifying the user: " + err);
-                            });
-                    });
-            })
-            .catch(() => {
-                reject(`Unable to find user: ${userData.userName}`);
-            });
-    });
+        const match = await bcrypt.compare(userData.password, users[0].password);
+        if (!match) throw `Incorrect Password for user: ${userData.userName}`;
+
+        // update login history
+        if (users[0].loginHistory.length === 8) users[0].loginHistory.pop();
+
+        users[0].loginHistory.unshift({
+            dateTime: new Date().toString(),
+            userAgent: userData.userAgent,
+        });
+
+        await User.updateOne(
+            { userName: users[0].userName },
+            { $set: { loginHistory: users[0].loginHistory } }
+        );
+
+        return users[0];
+    } catch (err) {
+        if (typeof err === "string") throw err;
+        throw "There was an error verifying the user: " + err;
+    }
 };
